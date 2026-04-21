416b246eb90befac6f7b08eb604d7349674912b6
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-04-21 12:45:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to react code javascript, You are almost there for testing!
+          Welcome to react code javascript, You are almost there for testing for lambda changes!
           Adding the versioning changes to updated branches of react javascript
         </p>
         <a
