3f2455d51abb65ebd9bb2ba2363e6e6491dd1826
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-04-23 14:56:00 +0530

    bugfix/test23 changes Update App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to react javascript, You are almost there for testing!
-          Adding the versioning changes to updated branches of react javascript
+          updating the deployment with version tags and with different namespaces!
         </p>
         <a
           className="App-link"
