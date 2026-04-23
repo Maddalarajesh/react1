a76135b97c2ce7f7e06f5e4eaa832f7df23f808a
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-04-23 17:41:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome!
+          Welcome to native!
         </p>
         <a
           className="App-link"
