30789f73d3726ce1fd01959078c0fecb03fa2703
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-04-24 13:02:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to react javascript, You are almost there for testing!
-          Adding the versioning changes to updated branches of react 
         </p>
         <a
           className="App-link"
