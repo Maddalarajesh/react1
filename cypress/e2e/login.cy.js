e65e86fd5af11e8a65c9a49bbd00b0987adfa833
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-05-13 18:06:43 +0530

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,13 @@
 describe('React App Test', () => {
 
     it('opens homepage', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('https://dev.deepcarve.online')
 
         cy.title().should('exist')
     })
 
     it('checks login button', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('https://dev.deepcarve.online')
 
         cy.get('#login-btn')
             .should('be.visible')
